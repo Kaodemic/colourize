ccdbf17c5803b506d217301c898c88dc9f1a5ba1
Tran Khanh <tran_khanh@outlook.com>
2021-05-12 10:08:17 +0700

    WIP

--- README.md
+++ README.md
@@ -3,3 +3,43 @@ byEasy to get start: just kick off the ./kaopy.ps1
 ```ps1
 ./kaopy.ps1
 ```
+
+# Desktop_Colourizer
+Pick and apply a wallpaper based on the user's background.
+<br>
+For best experience,
+Settings > Personalization > Colors > check the *Automatically pick an accent color from my background* box.
+## Launching the app
+Windows:
+<br>
+  &nbsp;&nbsp;&nbsp;Run Desktop_Colourizer.bat
+<br>
+Linux:
+<br>
+  &nbsp;&nbsp;&nbsp;chmod +x Desktop_Colourizer.sh
+<br>
+  &nbsp;&nbsp;&nbsp;./Desktop_Colourizer.sh
+<br>
+or
+<br>
+run *python main.py*
+
+## Updating the set of wallpapers
+Windows:
+<br>
+  &nbsp;&nbsp;&nbsp;Run Update_Wallpapers.bat
+<br>
+Linux:
+<br>
+  &nbsp;&nbsp;&nbsp;chmod +x Update_Wallpapers.sh
+<br>
+  &nbsp;&nbsp;&nbsp;./Update_Wallpapers.sh
+<br>
+or
+<br>
+run *python update.py*
+
+## Dependencies
+This was built on Windows 10 x64 and is intended for the same.
+<br>
+Run *pip install -r requirements.txt* to install all the dependencies.
--- main.py
+++ main.py
@@ -10,11 +10,44 @@ __license__ = "MIT"
 import argparse
 from logzero import logger
 
+import cv2
+import json
+import numpy as np
+from average import averagePixels
+from set_wall import apply_wallpaper
+from findFace import find_any_face
+
 
 def main(args):
     """ Main entry point of the app """
     logger.info("hello world")
     logger.info(args)
+
+    try:
+    pix = averagePixels( find_any_face() )
+    fb , fg , fr = pix.split(',')
+
+    with open('Wallpapers_lookup.json') as f:
+        Wallpapers_lookup = {} or json.load(f)
+    
+    THRESHOLD = 64
+
+    for val in Wallpapers_lookup:
+        ib, ig , ir = Wallpapers_lookup[ val ].split(',')
+        dif = abs( int( fb ) - int( ib ) ) + abs( int( fg ) - int( ig ) ) + abs ( int( fr ) - int( ir ) )
+
+        if dif < THRESHOLD:
+            # print( val )
+            apply_wallpaper( f'Wallpapers/{val}' )
+            break
+    else:
+        val = random.choice( list ( Wallpapers_lookup ) )
+        # print( val )
+        apply_wallpaper( f'Wallpapers/{val}' )
+        print("No match found hence a random wallpaper is being applied")
+        
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
